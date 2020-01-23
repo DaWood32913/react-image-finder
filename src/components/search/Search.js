53946cd113162fd210bfa718eae7416ecc3e99bd
DaWood32913 <woodleyjohnson@yahoo.com>
2020-01-23 14:41:09 -0500

    imported select field and menu items from material-ui. set up search bar to imput seatch params

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
-import SelectField from 'material-ui/SelectField';
+import React, { Component } from "react";
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
 
 class Search extends Component {
-    state = {
-        searchText: '',
-        amount: 15,
-        apiUrl: 'https://pixabay.com/api',
-        apiKey: process.env.REACT_APP_API_APIKEY,
-        images: []
-    }
+  state = {
+    searchText: "",
+    amount: 15,
+    apiUrl: "https://pixabay.com/api",
+    apiKey: process.env.REACT_APP_API_APIKEY,
+    images: []
+  };
+
+onTextChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+}
 
   render() {
     return (
       <div>
-        <TextField 
-         name="searchText"
-         value={this.state.searchText}
-         onChange={this.onTextChange}
-         floatingLabelText="Search For Images"
-         fullWidth={true}
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For Images"
+          fullWidth={true}
         />
+        <br />
+        <SelectField
+          name="amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
+        <br />
       </div>
-    )
+    );
   }
 }
 
